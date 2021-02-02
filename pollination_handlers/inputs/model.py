6202d546ceb902df8e2471e31f8f8592081e7ec1
Chris Mackey <chris@ladybug.tools>
2021-02-02 17:40:47 -0500

    feat(handlers): Add several handlers for energy simulation

--- pollination_handlers/inputs/model.py
+++ pollination_handlers/inputs/model.py
@@ -1,8 +1,9 @@
-"""Handlers for honeybee model."""
+"""Handlers for honeybee and dragonfly models."""
 import os
 import json
 
 from honeybee.model import Model
+from dragonfly.model import Model as ModelDF
 
 from .helper import get_tempfile
 
@@ -12,8 +13,8 @@ def model_to_json(model_obj):
 
         Args:
             model_obj: Either a Honeybee model or the path to the HBJSON file.
-                In case the model_obj is a path it will be returned as is. For HBModels
-                the model will be saved to a HBJSON file in a temp folder.
+                In case the model_obj is a path, it will be returned as is. For a
+                Model object, it will be saved to a HBJSON file in a temp folder.
 
         Returns:
             str -- Path to HBJSON file.
@@ -24,16 +25,44 @@ def model_to_json(model_obj):
         hb_file = model_obj
     elif isinstance(model_obj, Model):
         hb_file = get_tempfile('hbjson')
-        try:
-            obj_dict = model_obj.to_dict(abridged=True)
-        except TypeError:  # no abridged option
-            obj_dict = model_obj.to_dict()
+        obj_dict = model_obj.to_dict()
 
         # write the dictionary into a file
         with open(hb_file, 'w') as fp:
             json.dump(obj_dict, fp)
     else:
         raise ValueError(
-            'Model input should be a string or a Honeybee Model not a ' + type(model_obj)
+            'Model input should be a string or a Honeybee Model. '
+            'Not {}.'.format(type(model_obj))
         )
     return hb_file
+
+
+def model_dragonfly_to_json(model_obj):
+    """Translate a Dragonfly model to a DFJSON file.
+
+        Args:
+            model_obj: Either a Dragonfly model or the path to the DFJSON file.
+                In case the model_obj is a path, it will be returned as is.  For a
+                Model object, it will be saved to a DFJSON file in a temp folder.
+
+        Returns:
+            str -- Path to DFJSON file.
+    """
+    if isinstance(model_obj, str):
+        if not os.path.isfile(model_obj):
+            raise ValueError('Invalid file path: %s' % model_obj)
+        df_file = model_obj
+    elif isinstance(model_obj, ModelDF):
+        df_file = get_tempfile('dfjson')
+        obj_dict = model_obj.to_dict()
+
+        # write the dictionary into a file
+        with open(df_file, 'w') as fp:
+            json.dump(obj_dict, fp)
+    else:
+        raise ValueError(
+            'Model input should be a string or a Dragonfly Model. '
+            'Not {}.'.format(type(model_obj))
+        )
+    return df_file
