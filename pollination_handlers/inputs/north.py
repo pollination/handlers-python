c0f938fe98b0a948e5751de1ceda308c9fcdf865
Chris Mackey <chris@ladybug.tools>
2021-02-11 16:55:37 -0500

    fix(north): Catch the case of CAD vectors via duck typing

--- pollination_handlers/inputs/north.py
+++ pollination_handlers/inputs/north.py
@@ -15,14 +15,16 @@ def north_vector_to_angle(value):
         Returns:
             float -- north angle in degrees.
     """
-    if isinstance(value, Vector2D):
+    if hasattr(value, 'X') and hasattr(value, 'Y'):
+        vec = Vector2D(value.X, value.Y)
+        north = math.degrees(vec.angle_clockwise(Vector2D(0, 1)))
+    elif isinstance(value, Vector2D):
         north = math.degrees(value.angle_clockwise(Vector2D(0, 1)))
     else:
         try:
             north = float(value)
         except (ValueError, TypeError):
             raise ValueError(
-                'north input should be a number or a Vector2D. '
-                'Not {}.'.format(type(value))
+                'north input should be a number or a vector. Not {}.'.format(type(value))
             )
     return north
