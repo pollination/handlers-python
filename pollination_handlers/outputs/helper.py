deb1d319b527f678484bea26400b2022cda5fd11
Mostapha <sadeghipour@gmail.com>
2021-04-14 03:48:32 -0400

    fix(read rad results): add support for partial file read
    This commit add support for cases that there are several grids with the same identifier and they share the same results file.

--- pollination_handlers/outputs/helper.py
+++ pollination_handlers/outputs/helper.py
@@ -9,7 +9,7 @@ def read_sensor_grid_result(result_folder, extension, grid_key, is_percent=True)
         result_folder: Path to the folder containing the results.
         extension: Text for the file extension to be read (eg. res).
         grid_key: Text for the key in the grids_info.json that identifies the
-            file name of each sensor grid.
+            file name of each sensor grid. It is either identifier or group_identifier.
         is_percent: Boolean to note if the values are intended to be percent, in
             which case a check will be done to ensure no value is greater than
             one hundred.
@@ -29,12 +29,21 @@ def read_sensor_grid_result(result_folder, extension, grid_key, is_percent=True)
         grid_list = json.load(json_file)
     results = []
     for grid in grid_list:
-        result_file = os.path.join(
-            result_folder, '{}.{}'.format(grid[grid_key], extension))
-        if os.path.isfile(result_file):
-            with open(result_file) as inf:
-                if is_percent:
-                    results.append([min(float(line), 100) for line in inf])
-                else:
-                    results.append([float(line) for line in inf])
+        grid_id = grid[grid_key]
+        sensor_count = grid['count']
+        try:
+            st_ln = grid['start_ln']
+        except KeyError:
+            # older version of sensor info
+            st_ln = 0
+
+        result_file = os.path.join(result_folder, '{}.{}'.format(grid_id, extension))
+        with open(result_file) as inf:
+            for _ in range(st_ln):
+                next(inf)
+            if is_percent:
+                results.append([min(float(next(inf)), 100) for _ in range(sensor_count)])
+            else:
+                results.append([float(next(inf)) for _ in range(sensor_count)])
+
     return results
