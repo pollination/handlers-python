d8bf91a087d11730adf0b802c6c2e1d96977d065
Mikkel Pedersen <mkklpdrsn@gmail.com>
2023-07-13 22:08:48 +0200

    fix(postprocess): Add handler for custom grid metrics

--- pollination_handlers/inputs/postprocess.py
+++ pollination_handlers/inputs/postprocess.py
@@ -1,6 +1,8 @@
 """Handlers for post-processing options."""
 import json
 
+from .helper import get_tempfile
+
 
 def grid_metrics(json_file):
     with open(json_file) as file:
@@ -10,14 +12,17 @@ def grid_metrics(json_file):
     for grid_metric in grid_metrics:
         assert isinstance(grid_metric, dict), \
         'Each item in grid metrics must be a dictionary.'
-    for grid_metric in grid_metrics:
         if 'allOf' in grid_metric:
-            assert isinstance(grid_metric['allOff'], list)
-            for gr_m in grid_metric['allOff']:
+            assert isinstance(grid_metric['allOf'], list)
+            for gr_m in grid_metric['allOf']:
                 assert isinstance(gr_m, dict)
         elif 'anyOf' in grid_metric:
             assert isinstance(grid_metric['anyOf'], list)
             for gr_m in grid_metric['anyOf']:
                 assert isinstance(gr_m, dict)
 
-    return json_file
+    file_path = get_tempfile('json', 'grid_metrics')
+    with open(file_path, 'w') as f:
+        json.dump(grid_metrics, f)
+
+    return file_path
