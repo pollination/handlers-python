86a3a4c58b8110fddc302588770495786484f948
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2025-04-10 12:50:42 -0400

    fix(radiance params): return the original value

--- pollination_handlers/inputs/radiancepar.py
+++ pollination_handlers/inputs/radiancepar.py
@@ -8,18 +8,21 @@ from honeybee_radiance_command.options.rfluxmtx import RfluxmtxOptions
 def validate_rpict_params(input_params):
     rpict_options = RpictOptions()
     rpict_options.update_from_string(input_params, raise_error=True)
-
+    return input_params
 
 def validate_rtrace_params(input_params):
     rtrace_options = RtraceOptions()
     rtrace_options.update_from_string(input_params, raise_error=True)
+    return input_params
 
 
 def validate_rcontrib_params(input_params):
     rcontrib_options = RcontribOptions()
     rcontrib_options.update_from_string(input_params, raise_error=True)
+    return input_params
 
 
 def validate_rfluxmtx_params(input_params):
     rfluxmtx_options = RfluxmtxOptions()
     rfluxmtx_options.update_from_string(input_params, raise_error=True)
+    return input_params
